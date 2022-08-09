3c32db08c2f126ebb6c4fec4c8eb23f9add80781
“Marijus84” <marijus.romanovas@hotmail.com>
2022-08-09 23:00:55 +0300

    test post res

--- routes/weekThree/index.js
+++ routes/weekThree/index.js
@@ -1,23 +1,27 @@
-const path = require('path');
-const { NAME } = require(path.resolve(__dirname, 'constants'));
+const path = require("path");
+const { NAME } = require(path.resolve(__dirname, "constants"));
 const {
   generateSelected,
   generateTranslations,
   generatePartyGuests,
   getMeme,
-} = require(path.resolve(__dirname, 'utils'));
+} = require(path.resolve(__dirname, "utils"));
 
-const routes = app => {
+const routes = (app) => {
   app.get(`/${NAME}/selected`, (req, res) => res.json(generateSelected()));
 
-  app.get(`/${NAME}/translations`, (req, res) => res.json(generateTranslations()));
+  app.get(`/${NAME}/translations`, (req, res) =>
+    res.json(generateTranslations())
+  );
 
-  app.get(`/${NAME}/party`, (req, res) => res.json(generatePartyGuests('vip')));
-  app.get(`/${NAME}/wedding`, (req, res) => res.json(generatePartyGuests('plusOne')));
+  app.get(`/${NAME}/party`, (req, res) => res.json(generatePartyGuests("vip")));
+  app.get(`/${NAME}/wedding`, (req, res) =>
+    res.json(generatePartyGuests("plusOne"))
+  );
 
   app.get(`/${NAME}/meme`, (req, res) => res.json(getMeme()));
 
-  app.post(`/${NAME}/test-post`, (req, res) => res.json({ success: true, token: 'random-token' }));
+  app.post(`/${NAME}/test-post`, (req, res) => res.json(req));
 };
 
 module.exports = routes;
