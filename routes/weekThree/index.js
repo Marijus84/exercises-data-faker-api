a3596938d0a01f52be1065b18bf9561929d80f83
“Marijus84” <marijus.romanovas@hotmail.com>
2022-08-10 12:30:19 +0300

    as

--- routes/weekThree/index.js
+++ routes/weekThree/index.js
@@ -7,6 +7,15 @@ const {
   getMeme,
 } = require(path.resolve(__dirname, "utils"));
 
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "vqgjkxxmxlnuvq",
+  host: "ec2-54-155-110-181.eu-west-1.compute.amazonaws.com",
+  database: "d97np0smkrhvot",
+  password: "6b219d81fe0b192ca16706e6f72ff084c7d1f341eb92991f030ebb1a8289e45f",
+  port: 5432,
+});
+
 const routes = (app) => {
   app.get(`/${NAME}/selected`, (req, res) => res.json(generateSelected()));
 
