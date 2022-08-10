75bb45b2cb4c67e3bca4ffe6b722e22208166d28
“Marijus84” <marijus.romanovas@hotmail.com>
2022-08-10 14:19:24 +0300

    asdfas

--- routes/weekThree/index.js
+++ routes/weekThree/index.js
@@ -7,15 +7,25 @@ const {
   getMeme,
 } = require(path.resolve(__dirname, "utils"));
 
-const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: "vqgjkxxmxlnuvq",
-  host: "ec2-54-155-110-181.eu-west-1.compute.amazonaws.com",
-  database: "d97np0smkrhvot",
-  password: "6b219d81fe0b192ca16706e6f72ff084c7d1f341eb92991f030ebb1a8289e45f",
-  port: 5432,
-  ssl: { rejectUnauthorized: false },
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "vqgjkxxmxlnuvq",
+//   host: "ec2-54-155-110-181.eu-west-1.compute.amazonaws.com",
+//   database: "d97np0smkrhvot",
+//   password: "6b219d81fe0b192ca16706e6f72ff084c7d1f341eb92991f030ebb1a8289e45f",
+//   port: 5432,
+// });
+
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: `postgres://vqgjkxxmxlnuvq:6b219d81fe0b192ca16706e6f72ff084c7d1f341eb92991f030ebb1a8289e45f@ec2-54-155-110-181.eu-west-1.compute.amazonaws.com:5432/d97np0smkrhvot`,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+client.connect();
 
 const routes = (app) => {
   app.get(`/${NAME}/selected`, (req, res) => res.json(generateSelected()));
@@ -26,7 +36,7 @@ const routes = (app) => {
 
   app.get(`/${NAME}/party`, (req, res) => {
     res.json(
-      pool.query("SELECT * FROM test", (error, results) => {
+      client.query("SELECT * FROM test", (error, results) => {
         if (error) {
           console.log(error);
           throw error;
